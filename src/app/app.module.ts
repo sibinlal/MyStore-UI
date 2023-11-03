fe7832792b6e2cef323514f2a605c86563b53bbb
sibinlal <sbnlalpnr@gmail.com>
2023-11-03 20:51:31 +0530

    header added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/header/header.component';
+import { CartComponent } from './component/cart/cart.component';
+import { ProductComponent } from './component/product/product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    CartComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
